5798bf4ccc80a07edf772f1724dd854b61d0e930
Kytoon <eryk.olsza@gmail.com>
2025-05-08 00:21:50 +0200

    home page improvements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
 import SearchBar from "@/components/searchBar";
 import { Link } from "expo-router";
-import { ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, ScrollView, Text, View } from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { getPosts } from "@/services/api";
+import PostCard from "@/components/postCard";
 
 export default function Index() {
   const router = useRouter();
 
-  
+  const {
+    data: posts,
+    loading: postsLoading,
+    error: postsError,
+  } = useFetch(()=>getPosts({query: ''}))
   return (
     <View className="flex-1 bg-primary">
       <ScrollView className="flex-1 px-5" 
@@ -14,12 +21,36 @@ export default function Index() {
       contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
         <Text className="w-12 h-10 mt-20 mb-5 mx-auto text-center text-4xl">📚</Text>
 
+        {postsLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+        ):
+        postsError ? (
+          <Text>Error: {postsError?.message}</Text>
+        ):
         <View className="flex-1 mt-5">
           <SearchBar
           onPress={() => router.push("/search")}
           placeholder="Search for a movie"
           />
+
+          <>
+            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Posts</Text>
+
+            <FlatList
+              data={posts}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{ justifyContent: "flex-start", gap:20, paddingRight: 10, marginBottom:10 }}
+              className="mt-2 pb-32"
+              scrollEnabled={false}
+              renderItem={({ item }) => (
+                <PostCard {...item}/>
+              )}
+              />
+          </>
         </View>
+          }
+        
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -17,8 +17,13 @@ export const getPosts = async ({query}: {query: string}) => {
     if (!response.ok) {
         throw new Error('Network response was not ok')
     }
-    const data = await response.json()
-    return data
+    const posts = await response.json()
+    const postsWithThumbnails = posts.map((post: any) => {
+        const thumbnailUrl = `https://picsum.photos/id/${post.id}/200/300`;
+        return { ...post, thumbnailUrl };
+    });
+
+    return postsWithThumbnails;
 }
 
 export const getPhotosByPost = async ({postId, photoId}: {postId:number, photoId?: number}) => {
